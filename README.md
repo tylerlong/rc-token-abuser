681e6b5fa726fc3207898b46c1d91cab42c02b23
Tyler Long <tyler4long@gmail.com>
2023-10-18 10:38:38 -0700

    First working version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Untitled App
+# RingCentral Token Abuser
+
+For testing/troubleshooting purpose only.
+
+There is a "5 active tokens per user per app" limit. If you try to create more, the old ones will be revoked.
+
+This app creates lots of tokens to trigger the limit.
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Hello world!');
+import RingCentral from '@rc-ex/core';
+import waitFor from 'wait-for-async';
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const main = async () => {
+  for (let i = 0; i < 1000000; i++) {
+    await rc.authorize({ jwt: process.env.RINGCENTRAL_JWT_TOKEN! });
+    await waitFor({ interval: 5000 });
+    console.log(`${i + 1} token(s) created.`);
+  }
+};
+
+main();
